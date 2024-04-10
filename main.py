0a57c57b9edd7d20fb2362a64b67ec1c06113fdc
QuincyEzizio2018/QCode <princequincy18@gmail.com>
2024-04-10 00:31:56 -0700

    Created main frame or window frame

---
+++ main.py
@@ -0,0 +1,77 @@
+from tkinter import*
+
+main_screen = Tk()
+main_screen.title("JMIC Player")
+# main_screen.geometry("700x700")
+main_screen.minsize(850,750)
+main_screen.configure(bg="#0f1a2b")
+main_screen.resizable(False, False)
+
+
+def open_folder():
+    pass
+
+def scan_folder():
+    pass
+
+
+def play_pause():
+    pass
+
+
+def continue_play():
+    pass
+
+
+def nxt():
+    pass
+
+
+def prev():
+    pass
+
+
+def update_volume_and_label(x):
+    pass
+
+# Function to play the selected music
+def play_selected_music(event):
+   pass
+
+
+def get_album_cover(song_name, n):
+   pass
+
+
+def fetch_lyrics(artist, title):
+    pass
+    
+# Function to fetch and display lyrics from the API
+def showlyrics():
+    pass
+
+#Other Frames inside main_screen window or frame
+
+#Lyrics Scroll Bar
+
+
+#Playlist Frame and title_label
+
+
+#Playlist Scroll Bar
+
+
+#Label of Music being played
+
+ 
+#icon
+# image_icon = PhotoImage(file="") #this is for the top logo close to JMIC - Player
+# # main_screen.iconphoto(False, image_icon)
+
+#Button
+
+#Volume nub
+
+
+
+main_screen.mainloop()
