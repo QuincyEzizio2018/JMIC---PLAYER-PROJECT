03d921db735d07c041025170fe3d42db173d0486
QuincyEzizio2018/QCode <princequincy18@gmail.com>
2024-04-19 07:28:17 -0700

    new interface design

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from tkinter import*
-from tkinter import ttk
+from tkinter import ttk, filedialog, messagebox
+from pygame import mixer
 import customtkinter
+from PIL import Image, ImageTk
+import os
+import requests, time, random, io, pygame
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, APIC
 
 main_screen = Tk()
 main_screen.title("JMIC Player")
-# main_screen.geometry("700x700")
-main_screen.minsize(850,750)
-main_screen.configure(bg="#0f1a2b")
+
+# Get the screen width and height
+screen_width = main_screen.winfo_screenwidth()
+screen_height = main_screen.winfo_screenheight()
+
+# Calculate the window size relative to the screen
+window_width = int(screen_width * 0.45)  # 80% of screen width
+window_height = int(screen_height * 0.6)  # 60% of screen height
+
+# Calculate the window position relative to the screen
+x_pos = int((screen_width - window_width) / 2)  # Center horizontally
+y_pos = int((screen_height - window_height) / 2)  # Center vertically
+
+# Set window size and position
+main_screen.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
+
+main_screen.configure(bg="white")
 main_screen.resizable(False, False)
 
 
@@ -125,24 +145,71 @@ On Red : Ifeanyi
 5. Music time status
 """
 
+
+
 """MR QUINCY"""
 """
 On Black : Quincy
-1. Progress bar slider
-2. Pause button
-3. Play button
-4. Stop button
-5. Next button
-6. Previous button
-7. Repeat button
-8. Shuffle button
-9. Volume label
-10. Volume slider
+1. Music label
+2. Progress bar slider
+3. Music time status
+4. Shuffle button
+5. Previous button
+6. Pause button
+7. Play button
+8. Stop button
+9. Next Button
+10. Repeat button
+11. Volume label
+12. Volume slider
 """
-# Create Music Position Slider
-my_slider = ttk.Scale(main_screen, from_=0, to=100, orient=HORIZONTAL, value=0, command=slide, length=430, style="TScale")
-my_slider.place(x=380, y=530)
 
+#Button
+shuffle = PhotoImage(file="images/40x26/shuffle.png", width=40, height=26)
+Button(main_screen, text="", compound=LEFT, image=shuffle, fg= "white", bg="white", bd=0, font=("ariel", 10, "bold"), command=shuffle_playlist).place(x=235, y=465)
+
+previous_button = PhotoImage(file="images/40x40/previous.png", width=20, height=20)
+Button(main_screen, text="", compound=LEFT, image=previous_button, bd=0, fg= "white", bg="white", command=previous_song).place(x=260, y=460)
+
+pause_button = PhotoImage(file="images/40x40/pause.png", width=20, height=20)
+Button(main_screen, text="", compound=LEFT, image=pause_button, bd=0, fg= "white", bg="white", command=lambda: pause(paused)).place(x=290, y=460)
+
+play_button = PhotoImage(file="images/40x40/play.png", width=40, height=40)
+Button(main_screen, text="", compound=LEFT, image=play_button, bd=0, fg= "white", bg="white", command=play).place(x=320, y=450)
+
+stop_button = PhotoImage(file="images/40x40/stop.png", width=20, height=20)
+Button(main_screen, text="", compound=LEFT, image=stop_button, bd=0, fg= "white", bg="white", command=stop).place(x=370, y=460)
+
+next_button = PhotoImage(file="images/40x40/next.png", width=20, height=20)
+Button(main_screen, text="", compound=LEFT, image=next_button, bd=0, fg= "white", bg="white", command=next_song).place(x=400, y=460)
+
+repeat_img = PhotoImage(file="images/40x26/repeat.png", width=40, height=23)
+repeat = Button(main_screen, text="", bg="white", bd=0, image=repeat_img, compound=LEFT, fg= "white", font=("ariel", 10, "bold"), command=toggle_repeat)
+repeat.place(x=430, y=465)
+
+#Label of Music being played
+music_frame = Frame(main_screen, width=250, height=15, bd=4, bg="white").place(x=15, y=408)
+music = Label(music_frame, text="Current playing song", font=("ariel", 10), fg="Black", bg="white", wraplength=250)
+music.place(x=15, y=408)
+
+# Create Time Status Bar
+time_status_bar = Label(main_screen, text='00:00', bd=1, relief=GROOVE, anchor=E)
+time_status_bar.place(x=660, y=420, anchor="center")
+
+time_status_bar1 = Label(main_screen, text='00:00', bd=1, relief=GROOVE, anchor=E)
+time_status_bar1.place(x=290, y=420, anchor="center")
+
+#Volume nub
+volume_slider = customtkinter.CTkSlider(master=main_screen, from_=0, to=100, command=lambda value: [update_volume_and_label(value)], width=100, number_of_steps=100)
+volume_slider.place(x=582, y=460)
+
+volume_label = Label(main_screen, text="Volume: 100", font=("Helvetica", 7, "bold"), fg="black", bg="white")
+volume_label.place(x=602, y=475)
+
+
+# Create Music Position Slider
+my_slider = ttk.Scale(main_screen, from_=0, to=100, orient=HORIZONTAL, value=0, command=slide, length=320, style="TScale")
+my_slider.place(x=315, y=413)
 # Create a style object
 style = ttk.Style()
 style.theme_use("clam")
@@ -151,37 +218,6 @@ style.theme_use("clam")
 style.configure("TScale", troughcolor="light blue", background="blue", 
                 highlightcolor="sea green", slidercolor=[("active", "light green")], 
                 gripcount=0, bordercolor="light blue", relief="flat", sliderthickness = 15)
-
-
-#BUTTONS
-play_button = PhotoImage(file="images/70x70/Play.png", width=70, height=70)
-Button(main_screen, text="", compound=LEFT, image=play_button, fg= "white", bg="#0f1a2b", command=play).place(x=450, y=615)
-
-stop_button = PhotoImage(file="images/50x50/Stop.png", width=50, height=50)
-Button(main_screen, text="", compound=LEFT, image=stop_button, fg= "white", bg="#0f1a2b", command=stop).place(x=380, y=580)
-
-pause_button = PhotoImage(file="images/50x50/Pause.png")
-Button(main_screen, text="", compound=LEFT, image=pause_button, fg= "white", bg="#0f1a2b", command=lambda: pause(paused)).place(x=540, y=580)
-
-previous_button = PhotoImage(file="images/50x50/Previous.png", width=50, height=50)
-Button(main_screen, text="", compound=LEFT, image=previous_button, fg= "white", bg="#0f1a2b", command=previous_song).place(x=380, y=660)
-
-next_button = PhotoImage(file="images/50x50/Next.png", width=50, height=50)
-Button(main_screen, text="", compound=LEFT, image=next_button, fg= "white", bg="#0f1a2b", command=next_song).place(x=540, y=660)
-
-shuffle = PhotoImage(file="images/50x50/Shuffle.png", width=50, height=50)
-Button(main_screen, text="", compound=LEFT, image=shuffle, fg= "white", bg="#0f1a2b", font=("ariel", 10, "bold"), command=shuffle_playlist).place(x=620, y=580)
-
-repeat_img = PhotoImage(file="images/50x50/Repeat.png", width=50, height=50)
-repeat = Button(main_screen, text="", bg="#0f1a2b", image=repeat_img, compound=LEFT, fg= "white", font=("ariel", 10, "bold"), command=toggle_repeat)
-repeat.place(x=758, y=580)
-
-#VOLUME LABEL AND SLIDER
-volume_slider = customtkinter.CTkSlider(master=main_screen, from_=0, to=100, command=lambda value: [update_volume_and_label(value)], width=100, number_of_steps=100)
-volume_slider.place(x=660, y=690)
-
-volume_label = Label(main_screen, text="Volume: 100", font=("Helvetica", 10, "bold"), fg="white", bg="#0f1a2b")
-volume_label.place(x=670, y=660)
 
 
 
